061409ed5f7c60c784444421a5c9baa986804d7f
Elias <eliasgalindo@gmail.com>
2021-12-04 01:35:15 -0300

    Desafio-bonus 12 realizado - triangleCheck

--- src/challenges2.js
+++ src/challenges2.js
@@ -9,8 +9,19 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  let absolut1 = Math.abs(lineA - lineB);
+  let absolut2 = Math.abs(lineB - lineC);
+  let absolut3 = Math.abs(lineC - lineA);
+  let result = '';
+  if (lineA < (lineB + lineC) && lineA > absolut2 || lineB < (lineC + lineA) && lineB > absolut3) {
+    result = true;
+  } else if (lineC < (lineA + lineB) && lineC > absolut1) {
+    result = true;
+  } else {
+    result = false;
+  }
+  return result;
 }
 
 // Desafio 13
